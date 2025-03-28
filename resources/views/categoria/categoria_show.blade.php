fdcb13ed6fda201ab35a3c8698ec8dab93eb1efa
rikeffc <rikeffc@gmail.com>
2025-03-28 16:31:28 -0300

    i

---
+++ resources/views/categoria/categoria_show.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Categoria - Show</div>
+
+                <div class="card-body">
+                  <p><strong>Id: </strong>{{ $categoria->id }}</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//--------------------------------------
+//CATEGORIA
+//--------------------------------------
+
+Route::get('/categoria', [CategoriaController::class, 'index'])->name('categoria.index');
+Route::get('/categoria/{id}', [CategoriaController::class, 'show'])->name('categoria.show');
